da91d414b8c07557c8afacc9de7973ddeb09c7d9
Rajat Gundi <glrajat@gmail.com>
2021-06-01 00:31:48 +0530

    Refractored: payroll class to include gender and start date

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,12 +1,16 @@
 class EmployeePayroll{
     id;
     name;
+    gender;
     salary;
+    startDate;
 
-    constructor(id,name,salary){
-        this.id = id
-        this.name = name
-        this.salary = salary
+    constructor(...args){
+        this.id = args[0]
+        this.name = args[1]
+        this.gender = args[2]
+        this.salary = args[3]
+        this.startDate = args[4]
     }
 
 
@@ -18,9 +22,14 @@ class EmployeePayroll{
     set salary(salary){this.salary = salary}
 
     toString(){
-        return "id = "+this.id+", name = "+this.name+", salary = "+this.salary;
+        if(this.gender == undefined || this.startDate == undefined)
+            return "id = "+this.id+", name = "+this.name+", salary = "+this.salary;
+        else
+            return "id = "+this.id+", name = "+this.name+", gender = "+this.gender+", salary = "+this.salary+", start Date = "+this.startDate;
     }
 }
 
 let employee = new EmployeePayroll(1,"Rajat",50000)
+let employee2 = new EmployeePayroll(1,"Rajat G.L.",'M',30000,new Date())
 console.log(employee.toString())
+console.log(employee2.toString())
