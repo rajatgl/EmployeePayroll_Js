9be14e08d218466058b91b4e96884f37f3bac88b
Rajat Gundi <glrajat@gmail.com>
2021-06-01 16:53:55 +0530

    Added: UC13-regex check for employee-name

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,6 +1,5 @@
 class EmployeePayroll{
     id;
-    name;
     gender;
     salary;
     startDate;
@@ -8,28 +7,43 @@ class EmployeePayroll{
     constructor(...args){
         this.id = args[0]
         this.name = args[1]
-        this.gender = args[2]
-        this.salary = args[3]
+        this.salary = args[2]
+        this.gender = args[3]
         this.startDate = args[4]
     }
 
 
-    get id(){return this.id}
-    get name(){return this.name}
-    get salary(){return this.salary}
-
-    set name(name){this.name = name}
-    set salary(salary){this.salary = salary}
+    get name(){return this._name}
+    set name(name)
+    {   
+        let nameRegex = /^[A-Z]{1}[a-z]{3,}$/
+        if(nameRegex.test(name)){
+            this._name = name
+            return
+        }
+        else throw "Pattern mismatch"
+    }
 
     toString(){
         if(this.gender == undefined || this.startDate == undefined)
             return "id = "+this.id+", name = "+this.name+", salary = "+this.salary;
         else
-            return "id = "+this.id+", name = "+this.name+", gender = "+this.gender+", salary = "+this.salary+", start Date = "+this.startDate;
+            return "id = "+this.id+", name = "+this.name+", salary = "+this.salary+", gender = "+this.gender+", start Date = "+this.startDate;
     }
 }
 
 let employee = new EmployeePayroll(1,"Rajat",50000)
-let employee2 = new EmployeePayroll(1,"Rajat G.L.",'M',30000,new Date())
 console.log(employee.toString())
-console.log(employee2.toString())
+try{
+    employee.name = "ajath"
+    console.log(employee.toString())
+}catch(e){
+    console.error(e)
+}
+
+try{
+    let employee2 = new EmployeePayroll(2,"Rajat",'M',30000,new Date())
+    console.log(employee2.toString())
+}catch(e){
+    console.error(e)
+}
