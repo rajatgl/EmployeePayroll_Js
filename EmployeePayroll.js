6628ac695b551fa4fd2825e3eee15ece03952755
Rajat Gundi <glrajat@gmail.com>
2021-06-01 18:19:42 +0530

    Added: UC14- validation checks for id,salary and start-date

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,9 +1,5 @@
 class EmployeePayroll{
-    id;
-    gender;
-    salary;
-    startDate;
-
+    
     constructor(...args){
         this.id = args[0]
         this.name = args[1]
@@ -14,28 +10,57 @@ class EmployeePayroll{
 
 
     get name(){return this._name}
+    //UC13: regex check for name
     set name(name)
     {   
-        let nameRegex = /^[A-Z]{1}[a-z]{3,}$/
-        if(nameRegex.test(name)){
+        const NAME_REGEX = /^[A-Z]{1}[a-z]{3,}$/
+        if(NAME_REGEX.test(name)){
             this._name = name
             return
         }
-        else throw "Pattern mismatch"
+        else throw "Invalid Name"
+    }
+    //UC14: regex check for id
+    set id(id){
+        const ID_REGEX = /^[1-9]+[0-9]*/
+        if(ID_REGEX.test(id)){
+            this._id = id
+            return;
+        }
+        else
+            throw "Invalid employeeId"
+    }
+    //UC14: regex check for salary
+    set salary(salary){
+        const SALARY_REGEX = /^[1-9]+[0-9]*(\.[0-9]+)?/
+        if(SALARY_REGEX.test(salary))
+            this._salary = salary
+        else
+            throw "Invalid salary"
+    }
+    //regex check gender
+    set gender(gender){
+        const GENDER_REGEX = /^[F/M/undefined]{1}/
+        if(GENDER_REGEX.test(gender))
+            this._gender = gender
+        else
+            throw "Invalid gender" 
     }
 
+
     toString(){
-        if(this.gender == undefined || this.startDate == undefined)
+        if(this.gender == undefined && this.startDate == undefined)
             return "id = "+this.id+", name = "+this.name+", salary = "+this.salary;
-        else
-            return "id = "+this.id+", name = "+this.name+", salary = "+this.salary+", gender = "+this.gender+", start Date = "+this.startDate;
+        else{
+            const options = {day:'numeric',month:'numeric',year:'numeric'}
+            return "id = "+this.id+", name = "+this.name+", salary = "+this.salary+", gender = "+this.gender+", start Date = "+this.startDate.toLocaleDateString("en-IND",options);
+        }
     }
 }
 
-let employee = new EmployeePayroll(1,"Rajat",50000)
-console.log(employee.toString())
+
 try{
-    employee.name = "ajath"
+    let employee = new EmployeePayroll(2,"Rajat",50000.00)
     console.log(employee.toString())
 }catch(e){
     console.error(e)
